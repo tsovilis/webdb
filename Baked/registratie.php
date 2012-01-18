c73f29832b778c4c9e96d854e95f72cd47d9f935
Ruben de Vries <rubendevries@vpn-144-218.vpn.uva.nl>
2012-01-18 14:57:09 +0100

    nieuwe php

--- Baked/registratie.php
+++ Baked/registratie.php
@@ -7,15 +7,39 @@ if (!$con)
 
 mysql_select_db("webdb1247", $con);
 
-$sql="INSERT INTO Account (Emailadres,Wachtwoord,Voornaam,Tussenvoegsel,Achternaam,Postcode,Straatnaam,Huisnummer,Toevoeging,Telefoon) 
-VALUES		
-('$_POST[emailadres]','$_POST[wachtwoord]','$_POST[voornaam]','$_POST[tussenvoegselnaam]','$_POST[achternaam]','$_POST[postcode]','$_POST[straatnaam]','$_POST[postbusnummer]','$_POST[postbustoevoeging]','$_POST[telefoonnummer]')";
+	if($_POST[nieuwsbrief] == '1')
+			{
+			$nieuwsbrief = 1;;
+			}
+	else{
+	     $nieuwsbrief = 0;
+	    }
+	
+	if($_POST[emailadres] == $_POST[bevestig_emailadres]  && $_POST[wachtwoord] == $_POST[bevestig_wachtwoord])
+	    {
+		$sql="INSERT INTO Account (Emailadres,Wachtwoord,Voornaam,Tussenvoegsel,Achternaam,Postcode,Straatnaam,Huisnummer,Toevoeging,Telefoon,Nieuwsbrief) 
+		VALUES		
+		('$_POST[emailadres]','$_POST[wachtwoord]','$_POST[voornaam]','$_POST[tussenvoegselnaam]','$_POST[achternaam]','$_POST[postcode]','$_POST[straatnaam]','$_POST[postbusnummer]','$_POST[postbustoevoeging]','$_POST[telefoonnummer]','$nieuwsbrief')";
+
+	    }
+		
+	else	{ 
+		if($_POST[nieuwsbrief] == '1')
+			{
+			print"wel nieuwsbrief";
+			}
+		print "PENISSSSSSSS je wachtwoord of emeulsje is not de same";
+		}
+
+
 
 if (!mysql_query($sql,$con))
   {
   die('Error: ' . mysql_error());
   }
-echo "1 record added";
+
+header	("Location: accountBaked.html");
+
 
 mysql_close($con)
 ?>
