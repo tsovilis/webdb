192e28ae9d7168ec443fd8133713d27ee262dbec
Ruben de Vries <rubendevries@vpn-144-198.vpn.uva.nl>
2012-01-19 13:51:17 +0100

    status php

---
+++ Baked/status.php
@@ -0,0 +1,28 @@
+<?php
+$con = mysql_connect("localhost","webdb1247","9ru7raku");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("webdb1247", $con);
+		
+		$bestelling = $_POST["bestelling"];
+		$status = $_POST["status"];
+		
+
+		$sql="UPDATE Bestellingen SET BestelStatus = $status  WHERE Bestellingen_id = $bestelling";
+
+	    
+
+
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+
+header	("Location: hoi.php");
+
+
+mysql_close($con)
+?>
