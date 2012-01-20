5303c1b4ea87e146729ea9aacaa5be617eded2d9
Ivo Hoolwerf <hoolwerf@u018715.(none)>
2012-01-20 12:19:43 +0000

    bla

--- baked/taartinfoBaked.php
+++ baked/taartinfoBaked.php
@@ -92,7 +92,7 @@ obj.value=obj.value.substring(0,mlength)
 		echo "<table border='0' height='100%' width='100%'>";
 		echo "<tr height='180'>";
 		echo "<td width='100%-180px'> <center><h1>" . $row['Taartnaam'] . "</h1></center></td>";
-		echo "<td id= 'plaatjetaartinfo' width='180'><img src='images/".$row['Plaatje']."'alt='Taart'	width='180'	height='180'/> </td>";
+		echo "<td width='180'><img src='images/".$row['Plaatje']."'alt='Taart'	width='180'	height='180'/> </td>";
 		echo "</tr>";
 		echo "<tr>";
 		echo "<td id='langetaartinfo'>" .$row['BeschrijvingTaart']. "</td>";
@@ -100,17 +100,11 @@ obj.value=obj.value.substring(0,mlength)
 		mysql_close($con);
 		?>
 		
-		<td valign="top"> <center> <h3> Bestellen </h3> </center> </br>
+		<td id="bestellentaartinfo"> <center> <h3> Bestellen </h3> </center> </br>
 		<dl>
 			<form name="taartspecificaties" method="get"
 			<dt><center><h4>Oma's appeltaart</h4></center></dt>
-			<dd>Grootte:</dd> 	
-			<dd>	<select name="grootte">
-					<option value="klein">Klein</option>
-					<option value="medium" selected>Medium</option>
-					<option value="groot">Groot</option>
-					</select>
-			</dd>
+			
 			<dd>Kaarsjes:<i> <font size="1"> <br />(worden apart bijgeleverd)</font></i></dd>
 			<dd>			<select name="kaarsjes">
 							<option value="0">0</option>
@@ -125,8 +119,9 @@ obj.value=obj.value.substring(0,mlength)
 							<option value="1">45</option>
 							<option value="1">50</option>
 							</select></dd>
+			
 			<dd>Tekst:</dd>
-			<dd><textarea 	type="text" name="Tekst" cols="15" rows="2"
+			<dd><textarea 	type="text" name="Tekst" cols="13" rows="2"
 						maxlength="32" onkeyup="return ismaxlength(this)"
 						onfocus="if(this.value == 'Wat voor tekst wilt u erop? (max 32)') {this.value = '';}">Wat voor tekst wilt u erop? (max 32)</textarea></dd>
 			
@@ -138,6 +133,66 @@ obj.value=obj.value.substring(0,mlength)
 							<option value="4">4</option>
 							<option value="5">5</option>
 							</select></dd>
+							
+			<dd>Leverdatum:</dd>
+			<dd>	<i>Jaar:</i><br />
+					<select name="besteljaar">
+					<option value="2012">2012</option>
+					<option value="2013">2013</option>
+					<option value="2014">2014</option>
+					</select><br />
+										
+					<i>Maand:</i><br />
+					<select name="bestelmaand">
+					<option value="jan">Januari</option>
+					<option value="feb">Februari</option>
+					<option value="mrt">Maart</option>
+					<option value="apr">April</option>
+					<option value="mei">Mei</option>
+					<option value="jun">Juni</option>
+					<option value="jul">Juli</option>
+					<option value="aug">Augustus</option>
+					<option value="sep">September</option>
+					<option value="okt">Oktober</option>
+					<option value="nov">November</option>
+					<option value="dec">December</option>
+					</select><br />
+					
+					<i>Dag:</i><br />
+					<select name="besteldag">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
+					<option value="11">11</option>
+					<option value="12">12</option>
+					<option value="13">13</option>
+					<option value="14">14</option>
+					<option value="15">15</option>
+					<option value="16">16</option>
+					<option value="17">17</option>
+					<option value="18">18</option>
+					<option value="19">19</option>
+					<option value="20">20</option>
+					<option value="21">21</option>
+					<option value="22">22</option>
+					<option value="23">23</option>
+					<option value="24">24</option>
+					<option value="25">25</option>
+					<option value="26">26</option>
+					<option value="27">27</option>
+					<option value="28">28</option>
+					<option value="29">29</option>
+					<option value="30">30</option>
+					<option value="31">31</option>
+					</select><br />
+					</dd>
 			</form>
 		</dl>
 			<center><a href="winkelwagenBaked.html"><img src="images/bestellen1.png" alt="Bestellen"
